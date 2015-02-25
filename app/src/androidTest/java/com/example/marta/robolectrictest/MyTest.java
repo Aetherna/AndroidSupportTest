de0599ab4b93a0dab01d22ab94c7e8c9573ae3fe
Aetherna <mk.nowaczyk@gmail.com>
2015-02-25 11:13:44 +0100

    Initial commit

---
+++ app/src/androidTest/java/com/example/marta/robolectrictest/MyTest.java
@@ -0,0 +1,19 @@
+package com.example.marta.robolectrictest;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Created by Marta on 25/02/2015.
+ */
+@RunWith(AndroidJUnit4.class)
+public class MyTest {
+
+    @Test
+    public void testJUnit(){
+        Assert.assertTrue("jUnit works", true);
+    }
+}
